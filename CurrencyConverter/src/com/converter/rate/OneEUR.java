18cd8790029a4bf16047d1410946c68204bee1a9
Ayush Narule <127313182+Ayush3524@users.noreply.github.com>
2024-07-02 17:53:22 +0530

    Add files via upload

---
+++ CurrencyConverter/src/com/converter/rate/OneEUR.java
@@ -0,0 +1,20 @@
+package com.converter.rate;
+
+public class OneEUR {
+
+    public static double toUSD(){
+        return 1.07;
+    }
+
+    public static double toINR(){
+        return 89.67;
+    }
+
+    public static double toJPY(){
+        return 173.14;
+    }
+
+    public static double toGBP(){
+        return 0.85;
+    }
+}
